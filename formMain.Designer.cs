dea21b21fe1fd6cb6a1ddf576811999111b037db
Matt Harvey <matt@75central.com>
2020-05-15 13:05:53 -0500

    Initial commit

---
+++ formMain.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace ShortcutCleaner
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formMain));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.bClean = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.cleanNowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // bClean
+            // 
+            this.bClean.Location = new System.Drawing.Point(198, 286);
+            this.bClean.Name = "bClean";
+            this.bClean.Size = new System.Drawing.Size(181, 72);
+            this.bClean.TabIndex = 0;
+            this.bClean.Text = "Clean Now!";
+            this.bClean.UseVisualStyleBackColor = true;
+            this.bClean.Click += new System.EventHandler(this.bClean_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cleanNowToolStripMenuItem,
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(175, 100);
+            // 
+            // cleanNowToolStripMenuItem
+            // 
+            this.cleanNowToolStripMenuItem.Name = "cleanNowToolStripMenuItem";
+            this.cleanNowToolStripMenuItem.Size = new System.Drawing.Size(240, 32);
+            this.cleanNowToolStripMenuItem.Text = "Clean Now!";
+            this.cleanNowToolStripMenuItem.Click += new System.EventHandler(this.cleanNowToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(240, 32);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(240, 32);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TextBox1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox1.Location = new System.Drawing.Point(2, 12);
+            this.TextBox1.Multiline = true;
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.ReadOnly = true;
+            this.TextBox1.Size = new System.Drawing.Size(555, 114);
+            this.TextBox1.TabIndex = 3;
+            this.TextBox1.TabStop = false;
+            this.TextBox1.Text = "ShortcutCleaner is presented without warranty or guarantee and is licensed under " +
+    "the GNU General Public License v3.0";
+            this.TextBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 417);
+            this.Controls.Add(this.TextBox1);
+            this.Controls.Add(this.bClean);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "formMain";
+            this.Text = "ShortCut Cleaner";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.formMain_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button bClean;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem cleanNowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        internal System.Windows.Forms.TextBox TextBox1;
+    }
+}
+
---
+++ formMain.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+//using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ShortcutCleaner
+{
+    public partial class formMain : Form
+    {
+        public formMain()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Minimized;
+            
+
+        }
+
+        private void formMain_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 600000;
+            timer1.Start();
+            this.ShowInTaskbar = false;
+
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // TODO: Insert monitoring activities here.
+            clean();
+
+        }
+        private void clean()
+        {
+            //  eventLog1.WriteEntry("Monitoring the System", EventLogEntryType.Information, eventId++);
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string checkEx = @".lnk";
+
+            foreach (string fileName in Directory.GetFiles(path))
+            {
+                string extension = Path.GetExtension(fileName);
+                if (extension == checkEx)
+                {
+
+                    File.Delete(fileName);
+
+
+
+
+
+                }
+
+            }
+
+            path = Environment.GetFolderPath(Environment.SpecialFolder.CommonDesktopDirectory);
+            foreach (string fileName in Directory.GetFiles(path))
+            {
+                string extension = Path.GetExtension(fileName);
+                if (extension == checkEx)
+                {
+
+                   // File.Delete(fileName);
+
+
+
+
+
+                }
+
+            }
+        }
+
+        private void bClean_Click(object sender, EventArgs e)
+        {
+            clean();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+      
+
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void cleanNowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.clean();
+
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.Visible = true;
+            this.ShowInTaskbar = true;
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
